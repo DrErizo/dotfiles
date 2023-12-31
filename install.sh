321ad8c8dc764287fb6f84405b3116939a573d89
Erizo <78590012+DrErizo@users.noreply.github.com>
2023-12-31 13:16:21 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,13 +8,9 @@ cd paru
 makepkg -si
 cd ..
 
-paru -Sy git dunst mpd youtube-dl neofetch ncdu alacritty pcmanfm ranger exa base-devel mpv neovim clang ccls firefox flameshot volumeicon vimb azote wayland sway waybar wofi xorg-xwayland xorg-xlsclients qt5-wayland glfw-wayland zsh sddm
- 
-#these ones may or may not work on a fresh install idk too lazy to try
-#			    \/
-	sudo cp -r ./sddm /usr/share/sddm/
-	sudo sed -i 's/^Current=.*/Current=CleanWind/' /etc/sddm.conf
-#
+paru -Sy git dunst mpd youtube-dl neofetch ncdu alacritty pcmanfm ranger exa base-devel mpv neovim clang ccls firefox flameshot volumeicon vimb azote wayland sway waybar wofi xorg-xwayland xorg-xlsclients qt5-wayland glfw-wayland zsh sddm fastfetch nvim-packer-git 
+sudo cp -r ./sddm /usr/share/
+sudo sed -i 's/^Current=.*/Current=CleanWind/' /etc/sddm.conf
 
 sudo systemctl enable sddm
 sudo cp -r ./CyberSynchro /boot/grub/themes/
